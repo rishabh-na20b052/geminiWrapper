7c3c588d97accb55bfa7a7fe95810991239ebdbc
Rishabh Upadhyay na20b052 <na20b052@smail.iitm.ac.in>
2025-06-22 15:32:28 +0000

    it shows page not found

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  assetPrefix: '/geminiWrapper/',
+  assetPrefix: '/geminiWrapper',
   basePath: '/geminiWrapper',
   typescript: {
     ignoreBuildErrors: true,
